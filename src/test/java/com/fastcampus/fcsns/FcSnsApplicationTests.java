bf630f382469d7911865e0fd44a7fb604a35bf6a
dongeranguk <rooetym@naver.com>
2023-03-21 23:31:10 +0900

    #1-initialization

---
+++ src/test/java/com/fastcampus/fcsns/FcSnsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fastcampus.fcsns;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FcSnsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
