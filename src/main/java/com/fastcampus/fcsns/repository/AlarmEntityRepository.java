876f69f0207a2ec90cf3ac9afaf81acc8be6ae64
dongeranguk <rooetym@naver.com>
2023-04-13 23:58:34 +0900

    #12 - N + 1 문제 해결 알람 리스트를 가져올 때, 'user_id'을 조인 컬럼으로 알람을 발생시킨 'User' 정보를 가져온다. 이 때, 조인으로 'User' 정보를 한번에 가져오는 것을 기대했지만, JPA에서는 각각의 'User' 를 Select 하고 가져오는 'N+1 문제'가 발생하므로 이를 해결하기 위해 조인 컬럼 'user_id'에 fetch 옵션 'FetchType.LAZY'을추가하여 해결했다.

--- src/main/java/com/fastcampus/fcsns/repository/AlarmEntityRepository.java
+++ src/main/java/com/fastcampus/fcsns/repository/AlarmEntityRepository.java
@@ -1,14 +1,15 @@
 package com.fastcampus.fcsns.repository;
 
 import com.fastcampus.fcsns.model.entity.AlarmEntity;
-import com.fastcampus.fcsns.model.entity.UserEntity;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface AlarmEntityRepository extends JpaRepository<AlarmEntity, Integer> {
 
-    Page<AlarmEntity> findAllByUser(UserEntity user, Pageable pageable);
+    Page<AlarmEntity> findAllByUserId(@Param("user_id") Integer userId, Pageable pageable);
 }
