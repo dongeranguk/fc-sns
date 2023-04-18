0b12b42190330add6420f381b56b2328bf4216db
dongeranguk <rooetym@naver.com>
2023-04-18 23:21:50 +0900

    #13 - redis-impl redis를 사용하여 user 정보를 DB에서 바로 가져오지 않고, cache에서 먼저 조회한 뒤 DB에 접근하는 방식으로 DB IO를 줄일 수 있도록 구현하였다.

--- src/main/java/com/fastcampus/fcsns/model/User.java
+++ src/main/java/com/fastcampus/fcsns/model/User.java
@@ -1,8 +1,12 @@
 package com.fastcampus.fcsns.model;
 
 import com.fastcampus.fcsns.model.entity.UserEntity;
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import lombok.AllArgsConstructor;
+import lombok.Data;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
@@ -11,11 +15,13 @@ import java.sql.Timestamp;
 import java.util.Collection;
 import java.util.List;
 
-@Getter
+@Data
 @AllArgsConstructor
+@NoArgsConstructor
+@JsonIgnoreProperties
 public class User implements UserDetails {
     private Integer id;
-    private String userName;
+    private String username;
     private String password;
     private UserRole userRole;
     private Timestamp registeredAt;
@@ -23,31 +29,31 @@ public class User implements UserDetails {
     private Timestamp deletedAt;
 
     @Override
+    @JsonIgnore
     public Collection<? extends GrantedAuthority> getAuthorities() {
         return List.of(new SimpleGrantedAuthority(this.getUserRole().toString()));
     }
 
     @Override
-    public String getUsername() {
-        return this.userName;
-    }
-
-    @Override
+    @JsonIgnore
     public boolean isAccountNonExpired() {
         return this.deletedAt == null;
     }
 
     @Override
+    @JsonIgnore
     public boolean isAccountNonLocked() {
         return this.deletedAt == null;
     }
 
     @Override
+    @JsonIgnore
     public boolean isCredentialsNonExpired() {
         return this.deletedAt == null;
     }
 
     @Override
+    @JsonIgnore
     public boolean isEnabled() {
         return false;
     }
--- src/main/java/com/fastcampus/fcsns/service/UserService.java
+++ src/main/java/com/fastcampus/fcsns/service/UserService.java
@@ -6,6 +6,7 @@ import com.fastcampus.fcsns.model.Alarm;
 import com.fastcampus.fcsns.model.User;
 import com.fastcampus.fcsns.model.entity.UserEntity;
 import com.fastcampus.fcsns.repository.AlarmEntityRepository;
+import com.fastcampus.fcsns.repository.UserCacheRepository;
 import com.fastcampus.fcsns.repository.UserEntityRepository;
 import com.fastcampus.fcsns.util.JwtTokenUtils;
 import lombok.RequiredArgsConstructor;
@@ -26,6 +27,8 @@ public class UserService {
 
     private final BCryptPasswordEncoder encoder;
 
+    private final UserCacheRepository userCacheRepository;
+
     @Value("${jwt.secret-key}")
     private String secretKey;
 
@@ -33,8 +36,9 @@ public class UserService {
     private Long expiredTimeMs;
 
     public User loadUserByUserName(String userName) {
-        return userEntityRepository.findByUserName(userName).map(User::fromEntity).orElseThrow(() ->
-                new SnsApplicationException(ErrorCode.USER_NOT_FOUND, String.format("%s not founded", userName)));
+        return userCacheRepository.getUser(userName).orElseGet(() ->
+                userEntityRepository.findByUserName(userName).map(User::fromEntity).orElseThrow(() ->
+                new SnsApplicationException(ErrorCode.USER_NOT_FOUND, String.format("%s not founded", userName))));
     }
 
     @Transactional
@@ -53,10 +57,10 @@ public class UserService {
     // TODO : implement
     public String login(String userName, String password) {
         // 회원가입 여부 체크
-        UserEntity userEntity = userEntityRepository.findByUserName(userName).orElseThrow(() -> new SnsApplicationException(ErrorCode.USER_NOT_FOUND, String.format("%s not founded", userName)));
-
+        User user = loadUserByUserName(userName);
+        userCacheRepository.setUser(user);
         // 비밀번호 체크
-        if(!encoder.matches(password, userEntity.getPassword())) {
+        if(!encoder.matches(password, user.getPassword())) {
             throw new SnsApplicationException(ErrorCode.INVALID_PASSWORD);
         }
 
