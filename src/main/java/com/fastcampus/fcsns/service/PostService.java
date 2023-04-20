8a43b7b8b5492a39b2fe75febfdc233e44620038
dongeranguk <rooetym@naver.com>
2023-04-20 23:59:40 +0900

    #14 - sse-impl

--- src/main/java/com/fastcampus/fcsns/service/PostService.java
+++ src/main/java/com/fastcampus/fcsns/service/PostService.java
@@ -27,6 +27,7 @@ public class PostService {
     private final CommentEntityRepository commentEntityRepository;
 
     private final AlarmEntityRepository alarmEntityRepository;
+    private final AlarmService alarmService;
 
     @Transactional
     public void create(String title, String body, String userName) {
@@ -92,9 +93,8 @@ public class PostService {
 
         // save
         likeEntityRepository.save(LikeEntity.of(userEntity, postEntity));
-
-        alarmEntityRepository.save(AlarmEntity.of(postEntity.getUser(), AlarmType.NEW_LIKE_ON_POST, new AlarmArgs(userEntity.getId(), postEntity.getId())));
-
+        AlarmEntity alarmEntity = alarmEntityRepository.save(AlarmEntity.of(postEntity.getUser(), AlarmType.NEW_LIKE_ON_POST, new AlarmArgs(userEntity.getId(), postEntity.getId())));
+        alarmService.send(alarmEntity.getId(), postEntity.getUser().getId());
     }
 
     public Long likeCount(Integer postId) {
@@ -112,8 +112,8 @@ public class PostService {
 
         // comment save
         commentEntityRepository.save(CommentEntity.of(userEntity, postEntity, comment));
-
-        alarmEntityRepository.save(AlarmEntity.of(postEntity.getUser(), AlarmType.NEW_COMMENT_ON_POST, new AlarmArgs(userEntity.getId(), postEntity.getId())));
+        AlarmEntity alarmEntity = alarmEntityRepository.save(AlarmEntity.of(postEntity.getUser(), AlarmType.NEW_COMMENT_ON_POST, new AlarmArgs(userEntity.getId(), postEntity.getId())));
+        alarmService.send(alarmEntity.getId(), postEntity.getUser().getId());
 
     }
 
